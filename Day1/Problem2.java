69d3fd935cfecd945ecfc29188b50dda59a2b01d
= <=>
2026-06-30 14:12:29 +0530

    new

--- Day1/Problem2.java
+++ Day1/Problem2.java
@@ -1,5 +1,10 @@
 package Day1;
-
 public class Problem2 {
-    // /ofedhyksahfkhadkfhkahfkhadkjfhkjadhk
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("sum=" + sum);
+    }
 }
